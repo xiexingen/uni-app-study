224257cb0eda84b35e7993c24ef6fe05c2a90a30
xiexingen <1002275364@qq.com>
2020-10-17 09:18:54 +0800

    init

---
+++ pages/news/detail.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		dddd
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		onLoad(options){
+			console.log(options)
+		}
+	}
+</script>
+
+<style lang="less">
+
+</style>
---
+++ pages/news/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<text space="ensp" selectable="true">{{title}}</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title:'消息    页面1'
+			};
+		}
+	}
+</script>
+
+<style lang="less">
+
+</style>
